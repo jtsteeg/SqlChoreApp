82cce53c7e07e407332ef38f99ee1a63277df2a7
Jacob Steeg <jacob.t.steeg@gmail.com>
2021-10-18 15:32:26 -0500

    adds classes to disconnect/connect to DB, installs dependencies

---
+++ SqlChoreApp/ConnectToDB.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SqlChoreApp
+{
+    public class ConnectToDB
+    {
+                   
+        public void connect()
+        {
+            try
+            {
+                Console.WriteLine("Connect to SQL Server and demo Create, Read, Update and Delete operations.");
+
+                // Build connection string
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "DESKTOP-0SORUCL\\SQLSERVER2019";
+                builder.UserID = "sa";
+                builder.Password = "youshouldn'tsavepasswordsasplaintext!1";
+                builder.InitialCatalog = "master";
+
+                // Connect to SQL
+                Console.Write("Connecting to SQL Server ... ");
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Done.");
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
